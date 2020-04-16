b18bd78d4d5f8a4f0afb0622427a5d1f81161f44
Dennis <nndenninn@gmail.com>
2020-04-16 18:02:40 +0300

    Test

--- XIBSTest/ViewController.swift
+++ XIBSTest/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let xibView = UINib(nibName: "XibView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! XibView
+        xibView.frame = CGRect(x: view.frame.midX - 100, y: view.frame.midY - 100, width: 200, height: 200)
+        view.addSubview(xibView)
     }
-
-
 }
 
---
+++ XIBSTest/XibView.swift
@@ -0,0 +1,21 @@
+//
+//  XibView.swift
+//  XIBSTest
+//
+//  Created by Dennis Nesanoff on 16.04.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class XibView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

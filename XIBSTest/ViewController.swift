c631c107dc10645a1fc867fdbe6ae573ef877a2a
Dennis <nndenninn@gmail.com>
2020-04-16 17:32:13 +0300

    Initial Commit

---
+++ XIBSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XIBSTest
+//
+//  Created by Dennis Nesanoff on 16.04.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
